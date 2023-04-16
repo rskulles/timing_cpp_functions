86445c82ece825a20f1578335fa3a6927d656286
Roy S <rskulles at gmail dot com>
2023-04-15 23:48:21 -0400

    Changedd from std::vector to array.

--- main.cpp
+++ main.cpp
@@ -1,21 +1,23 @@
+#include <_types/_uint8_t.h>
 #include <chrono>
 #include <functional>
 #include <iostream>
 #include <vector>
 
-template <typename T, typename P, typename D>
-D time_func(std::function<T(P &)> func, P &param) {
+std::chrono::milliseconds
+time_func(std::function<uint(uint8_t **, size_t, size_t)> func, uint8_t **param,
+          size_t x, size_t y) {
   std::chrono::high_resolution_clock clock;
   auto tp1 = clock.now();
-  auto r = func(param);
+  auto r = func(param, x, y);
   auto tp2 = clock.now();
-  return std::chrono::duration_cast<D>(tp2 - tp1);
+  return std::chrono::duration_cast<std::chrono::milliseconds>(tp2 - tp1);
 }
 
-uint sum_matrix(std::vector<std::vector<uint8_t> > matrix) {
+uint sum_matrix(uint8_t **matrix, size_t x, size_t y) {
   uint sum = 0;
-  for (size_t i = 0; i < matrix.size(); ++i) {
-    for (size_t j = 0; j < matrix[0].size(); ++j) {
+  for (size_t i = 0; i < x; ++i) {
+    for (size_t j = 0; j < y; ++j) {
       sum += matrix[i][j];
     }
   }
@@ -25,20 +27,21 @@ uint sum_matrix(std::vector<std::vector<uint8_t> > matrix) {
 void time_size(size_t x, size_t y, size_t numb_of_times) {
   std::cout << "Creating a " << x << "x" << y << " matrix and running code "
             << numb_of_times << " times" << std::endl;
-  std::vector<std::vector<uint8_t> > matrix; 
 
-
+  uint8_t **matrix;
+  matrix = new uint8_t *[x];
   for (auto i = 0; i < x; ++i) {
-    matrix.push_back( std::vector<uint8_t>(y, 1));
+    matrix[i] = new uint8_t[y];
+    for (auto j = 0; j < y; ++j) {
+      matrix[i][j] = 1;
+    }
   }
-
   auto ms_avg = std::chrono::milliseconds::zero();
   auto ms_min = std::chrono::milliseconds::zero();
   auto ms_max = std::chrono::milliseconds::zero();
   std::cout << "Running...." << std::endl;
   for (auto i = 0; i < numb_of_times; ++i) {
-    auto d = time_func<uint, std::vector<std::vector<uint8_t> > ,
-                       std::chrono::milliseconds>(sum_matrix, matrix);
+    auto d = time_func(sum_matrix, matrix, x, y);
     if (i == 0) {
       ms_avg = d;
       ms_min = d;
@@ -61,6 +64,10 @@ void time_size(size_t x, size_t y, size_t numb_of_times) {
             << "\tslowest: " << ms_max.count() << "ms" << std::endl
             << "\tavg: " << ms_avg.count() / numb_of_times << "ms" << std::endl;
   std::cout << "**********" << std::endl << std::endl;
+  for (auto i = 0; i < x; ++i) {
+    delete[] matrix[i];
+  }
+  delete[] matrix;
 }
 
 int main() {
@@ -69,6 +76,6 @@ int main() {
   time_size(100, 1000, 1000);
   time_size(100, 10000, 1000);
   time_size(100, 100000, 1000);
-  time_size(100, 1000000, 100); 
+  time_size(100, 1000000, 100);
   return 0;
 }
